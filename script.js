6f8656e247e7a428694d3bf74d44299c804797dc
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-09 19:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-// Do not change the code below
-alert(stringChop(str, size));
-describe('My Test', () => {
-  it('should work', () => {
-    cy.visit(baseUrl, { 
-      onBeforeLoad(win) { 
-        // Stub your functions here 
-        cy.stub(win, "prompt").onFirstCall().returns('some string').onSecondCall().returns(5); 
-      } 
-    }); 
-
-    // Rest of your test code here
-	function stringChop(str, size) {
-		
-		let arr = [];
-	    for (let i = 0; i < str.length; i += parseInt(size)) {
-	        arr.push(str.slice(i, i + parseInt(size)));
+function stringChop(str, size) {
+	let arr = [];
+    for (let i = 0; i < str.length; i += parseInt(size)) {
+        arr.push(str.slice(i, i + parseInt(size)));
     }
     return arr;
 }
+
+
+// Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-    cy.on("window:alert", str => { 
-      expect(str.toString()).to.equal([].toString()); 
-    }); 
-  });
-});
+alert(stringChop(str, size));
