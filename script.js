af99f82de44507cabaa98f091fd994dd49d2894b
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-10 09:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function stringChop(str, size) {
 	let arr = [];
     for (let i = 0; i < str.length; i += parseInt(size)) {
-        arr.push(str.slice(i, i + parseInt(size)));
+		if(str === ""){
+			return arr;
+		}
+		else{
+	        arr.push(str.slice(i, i + parseInt(size)));
     }
+	}
     return arr;
 }
-
 
 // Do not change the code below
 const str = prompt("Enter String.");
